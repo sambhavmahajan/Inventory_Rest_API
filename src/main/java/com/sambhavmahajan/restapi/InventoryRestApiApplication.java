3637d0693cbc649d570e41fe4fa60575b6db5ae8
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-07-20 15:03:15 +0530

    Initial Commit

---
+++ src/main/java/com/sambhavmahajan/restapi/InventoryRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sambhavmahajan.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sambhavmahajan/restapi/InventoryRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sambhavmahajan.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
